8b0d8513c9eb06a0df83fde0ed74a3859ea56f4a
AL3XANDER <alexanderponte7@gmail.com>
2025-05-22 18:21:28 -0500

    configuracion de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
 });
